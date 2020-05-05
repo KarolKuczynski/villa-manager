1741f76f254f77355eb468ffba11bbf80fc6af25
karolkuczynski82 <karolkuczynski@o2.pl>
2020-05-05 20:30:18 +0200

    prepared simple project structure

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GuestListComponent } from './guests/guest-list/guest-list.component';
+import { RoomListComponent } from './rooms/room-list/room-list.component';
+import { HomeComponent } from './home/home.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'guests', component: GuestListComponent },
+  { path: 'rooms', component: RoomListComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,16 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GuestListComponent } from './guests/guest-list/guest-list.component';
+import { GuestEditComponent } from './guests/guest-edit/guest-edit.component';
+import { RoomListComponent } from './rooms/room-list/room-list.component';
+import { RoomEditComponent } from './rooms/room-edit/room-edit.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, GuestListComponent, GuestEditComponent, RoomListComponent, RoomEditComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
