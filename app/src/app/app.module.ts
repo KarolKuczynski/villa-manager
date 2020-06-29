74345bdca0088f3372d9573e9d3b01cf2b8f3d99
karolkuczynski82 <karolkuczynski@o2.pl>
2020-06-29 19:47:42 +0200

    missing file

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,8 +11,15 @@ import { RoomEditComponent } from './rooms/room-edit/room-edit.component';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, GuestListComponent, GuestEditComponent, RoomListComponent, RoomEditComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    GuestListComponent,
+    GuestEditComponent,
+    RoomListComponent,
+    RoomEditComponent,
+    HomeComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
