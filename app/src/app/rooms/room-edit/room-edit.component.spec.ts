da006a5b715a6c8eb020b03a3f7dcfcf99ff90a5
karolkuczynski82 <karolkuczynski@o2.pl>
2021-06-13 19:27:37 +0200

    update angular to version 11

--- app/src/app/rooms/room-edit/room-edit.component.spec.ts
+++ app/src/app/rooms/room-edit/room-edit.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { RoomEditComponent } from './room-edit.component';
 
@@ -6,7 +6,7 @@ describe('RoomEditComponent', () => {
   let component: RoomEditComponent;
   let fixture: ComponentFixture<RoomEditComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ RoomEditComponent ]
     })
