64a899f8d639fe7230f16286b28149e41b5a9884
Karol Kuczynski <karolkuczynski@o2.pl>
2021-06-26 22:57:35 +0200

    added basic design of main container

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,41 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GuestListComponent } from './guests/guest-list/guest-list.component';
-import { GuestEditComponent } from './guests/guest-edit/guest-edit.component';
-import { RoomListComponent } from './rooms/room-list/room-list.component';
-import { RoomEditComponent } from './rooms/room-edit/room-edit.component';
-import { HomeComponent } from './home/home.component';
 
-import { GuestService } from './_services/guest.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-
+import { HeaderModule } from './header/header.module';
+import { SharedModule } from './shared/shared.module';
+import { MenuModule } from './menu/menu.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    GuestListComponent,
-    GuestEditComponent,
-    RoomListComponent,
-    RoomEditComponent,
-    HomeComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
-
-   
-    
+    HeaderModule,
+    MenuModule,
+    SharedModule,
   ],
-  providers: [GuestService],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ app/src/app/menu/menu.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+
+import { SharedModule } from '../shared/shared.module';
+import { MenuComponent } from './menu.component';
+
+@NgModule({
+  declarations: [
+    MenuComponent
+  ],
+  imports: [
+    SharedModule
+  ],
+  exports: [MenuComponent]
+})
+export class MenuModule { }
